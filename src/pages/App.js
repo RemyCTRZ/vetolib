7de5ddde8c715a9050e9ef25605a3dfa0d15e3dc
Rémy <104489124+RemyCTRZ@users.noreply.github.com>
2022-09-07 16:55:51 +0200

    Contact créé

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,12 +2,13 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Accueil from "./Accueil";
 import Carnet from "./Carnet";
+import Contact from "./Contact";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Carnet />
+            <Contact />
             <Footer />
         </div>
     );
---
+++ src/pages/Contact.js
@@ -0,0 +1,21 @@
+import '../style/Contact.css';
+import { BsTelephoneFill, BsFacebook } from 'react-icons/bs';
+import { IoMdMail } from 'react-icons/io';
+
+function Contact() {
+    return (
+        <div className="Contact">
+            <p className='contact-titre'>Contact</p>
+            <article className='contact'>
+                <p className='contact-nom'>JEAN-MICHEL DUPONT</p>
+                <BsTelephoneFill />
+                <p>03.21.23.45.67</p>
+                <IoMdMail />
+                <p>jeanmichel@veto.fr</p>
+                <p className='infos-contact'>Du lundi au vendredi de 8h à 19h</p>
+            </article>
+        </div>
+    );
+}
+
+export default Contact;
