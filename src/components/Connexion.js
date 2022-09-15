e284c8edaa0280d073d6b8cd7c3fd79c1198d4c1
Rémy <104489124+RemyCTRZ@users.noreply.github.com>
2022-09-15 17:01:51 +0200

    Ajouter un animal

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -6,11 +6,11 @@ function Connexion() {
         <div className="Connexion" id="Connexion">
             <div className="connexion-email">
                 <p className="connexion-email-txt">Email :</p>
-                <input type="email" placeholder="exemple@mail.fr"></input>
+                <input type="email" required placeholder="exemple@mail.fr"></input>
             </div>
             <div className="connexion-mdp">
                 <p className="connexion-mdp-txt">Mot de passe :</p>
-                <input type="password"></input>
+                <input type="password" required></input>
             </div>
                 <NavLink className="link-inscription" to="/inscription">Inscription</NavLink>
                 <a className="mdp-oublie" href="/">Mot de passe oublié</a>
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
   const user = {
     id: 1,
     isAdmin: true,
-    isConnected: true,
+    isConnected: false,
     name: "Rémy"
   }
 
